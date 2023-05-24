90e08e6dffcaff1b2a2f3ccd4b7fddb36db2a765
sundruid <sundruid@users.noreply.github.com>
2023-05-24 12:09:35 -0700

    Some added error handling

--- hash_analysis.py
+++ hash_analysis.py
@@ -1,26 +1,32 @@
+#!/usr/bin/env python3
 
 import os
-import csv
 import collections
 from pathlib import Path
-
-# Read input file
-with open("FIMFILEA.OUT", "r") as file:
-    reader = csv.reader(file, delimiter='\t')
-    data = [row for row in reader]
 
 # Initialize directory counter
 dir_counter = collections.Counter()
 
-# Analyze the file paths and count changes
-for row in data:
-    date_code, file_path, sha256, has_changed = row
-    if has_changed.lower() == "true":
-        dir_path = os.path.dirname(file_path)
-        dir_counter[dir_path] += 1
+# Read input file
+with open("FIMFILEA.OUT", "r") as file:
+    lines = file.readlines()
+    print(f"Total lines in the file: {len(lines)}")  # Debugging print
+
+    for line in lines:
+        parts = line.strip().split("\t")
+        if len(parts) != 4:
+            print(f"Skipping line due to incorrect number of fields: {line}")  # Debugging print
+            continue
+
+        date_time, file_path, sha256, has_changed = parts
+
+        if has_changed == "TRUE":
+            dir_path = os.path.dirname(file_path)
+            dir_counter[dir_path] += 1
 
 # Calculate the total number of changes
 total_changes = sum(dir_counter.values())
+print(f"Total changes found: {total_changes}")  # Debugging print
 
 # Calculate the percentage of changes for each directory
 dir_percentages = {}
@@ -30,8 +36,9 @@ for dir_path, change_count in dir_counter.items():
 # Sort directories by the percentage of changes
 sorted_dirs = sorted(dir_percentages.items(), key=lambda x: x[1], reverse=True)
 
-# Write the analysis to the output file
-with open("analysis.txt", "w") as output:
-    writer = csv.writer(output, delimiter='\t')
-    for dir_path, percentage in sorted_dirs:
-        writer.writerow([dir_path, f"{percentage:.2f}%"])
+# Print the analysis to stdout
+for dir_path, percentage in sorted_dirs:
+    print(f"{dir_path}\t{percentage:.2f}%")
+
+# Print the number of directories analysed
+print(f"Number of directories analysed: {len(sorted_dirs)}")  # Debugging print
